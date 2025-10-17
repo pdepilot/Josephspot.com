99464b6ca0114839db912626a4f26558f61b9957
Princeton Swart <pdepilot@yahoo.com>
2025-10-17 12:22:01 +0100

    corrected video

--- JAVASCRIPT/gallery.js
+++ JAVASCRIPT/gallery.js
@@ -78,8 +78,6 @@ scrollBtn.onclick = function () {
 };
 
 
-
-
 // WhatsApp link
 const whatsappNumber = "2349064296917"; // Replace with your WhatsApp number (no '+' sign)
 const whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
